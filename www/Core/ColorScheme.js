a0bf751a9eb2e85a37f06814fc3dc19787892f03
Brian Olsen <olsenb@purdue.edu>
2015-11-04 16:10:28 -0500

    Updating Color Scheme to include linear interpolation
    Updating Color Scheme to include linear interpolation for values between
    color bin ranges.

--- www/Core/ColorScheme.js
+++ www/Core/ColorScheme.js
@@ -31,7 +31,53 @@
 		});
 		return numClasses;
     }
-    
+        
+	/**
+	* Returns an RGB interpolated value between two rgb values. 
+	*
+	* @param {Object} rgbA - rgb() tuple
+	* @param {Object} rgbB - rgb() tuple
+	* @param {Number} threshold - float between [0.0, 1.0]
+	* @param {function} interpolatorFn - interpolator function
+	* @return {Object} rbg
+	*/
+    function interpolateRgb(rgbA, rgbB, threshold, interpolatorFn) {
+        threshold = toArray(threshold, 3);
+        return {
+            r: ~~interpolatorFn(rgbA.r, rgbB.r, threshold[0]),
+            g: ~~interpolatorFn(rgbA.g, rgbB.g, threshold[1]),
+            b: ~~interpolatorFn(rgbA.b, rgbB.b, threshold[2])
+        };
+    }
+
+    /**
+     * Returns an interpolated value between two values. 
+     *
+     * @param {Number} valueA - color channel int value
+     * @param {Number} valueB - color channel int value
+     * @param {Number} threshold - float between [0.0, 1.0]
+     * @param {function} interpolatorFn - interpolator function
+     * @return {int}
+     */
+    function linear(valueA, valueB, threshold) {
+        return valueA * (1 - threshold) + valueB * threshold;
+    }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    function toArray(arr, size) {
+        var isNum = isNumeric(arr);
+        arr = !Array.isArray(arr) ? [arr] : arr;
+        for (var i = 1; i < size; i++) {
+            if (arr.length < size) {
+                arr.push(isNum ? arr[0] : 0);
+            }
+        }
+        return arr;
+    }
+
 
     var ColorScheme = function ColorScheme(options) {
         options = Util.defined(options) ? options : Util.defaultValue.EMPTY_OBJECT;
@@ -50,23 +96,38 @@
         this._stepValue = Util.defined(options.useOuterBoundaries) && options.useOuterBoundaries===true ? this._scale : (this.maxValue - this.minValue) / (this._numClasses - 2);
 
         this.getColorIndex = function (value) {
-            if (value <= this.minValue) {
-                return this._colorScheme[0];
+            if (value < this.minValue) {
+                return Cesium.Color.fromBytes(255, 255, 255, this._alphaValue);
             }
 
-            if (value >= this.maxValue) {
-                return this._colorScheme[this._numClasses - 1];
-            }
-            var colorIndex = Math.round((value - this.minValue) / this._scale);
-            if (colorIndex < 0) {
-                colorIndex = 0;
+            if (value > this.maxValue) {
+                return Cesium.Color.fromBytes(0, 0, 0, this._alphaValue);
             }
 
-            if (colorIndex > (this._numClasses - 1)) {
-                colorIndex = (this._numClasses - 1);
+            var binStepValue = (this.maxValue - this.minValue) / (this._numClasses - 1);
+            var index = Math.floor((value - this.minValue) / (binStepValue));
+            var weight = (((value - this.minValue) % binStepValue) / binStepValue);
+
+
+            if (index >= this._numClasses - 1) {
+                index = this._numClasses - 2;
+                weight = 1;
             }
 
-            return this._colorScheme[colorIndex];
+            var color1 = {
+                r: this._colorScheme[index].red * 255,
+                g: this._colorScheme[index].green * 255,
+                b: this._colorScheme[index].blue * 255
+            };
+            var color2 = {
+                r: this._colorScheme[index + 1].red * 255,
+                g: this._colorScheme[index + 1].green * 255,
+                b: this._colorScheme[index + 1].blue * 255
+            };
+
+            var rgb = interpolateRgb(color1, color2, weight, linear);
+
+            return Cesium.Color.fromBytes(rgb.r, rgb.g, rgb.b, this._alphaValue)
         };
 
         this.updateAlphaValue = function (value) {
@@ -80,19 +141,15 @@
 
         this.generateLegend = function (elementId, title) {
             var html = "<div class='" + elementId + "-title'>" + title + "</div>\n" +
-                "<div class='" + elementId + "-scale'>\n" +
-                "<ul class='" + elementId + "-labels'>\n";
+            "<div class='" + elementId + "-scale'>\n" +
+            "<ul class='" + elementId + "-labels'>\n";
 
             for (var i = 0; i <= this._numClasses - 1; i++) {
                 html += "<li><span style='background-color:" + this._colorScheme[i].toCssColorString() + ";'></span>";
-                if (i === 0) {
-                    html += "<= " + this.minValue.toFixed(this._precision).toString();
-                } else if (i < this._numClasses - 1) {
-                    html += (this.minValue + (this._stepValue * (i - 1)) + this._increment).toFixed(this._precision).toString() +
-                    " - " + (this.minValue + (this._stepValue * i)).toFixed(this._precision).toString();
-                } else {
-                    html += "> " + this.maxValue.toFixed(this._precision).toString();
-                }
+
+                html += (this.minValue + (this._stepValue * i) + (i === 0 ? 0 : this._increment)).toFixed(this._precision).toString() +
+                " - " + (this.minValue + (this._stepValue * (i + 1))).toFixed(this._precision).toString();
+
                 html += "</li>\n";
             }
             html += "</ul>\n";
